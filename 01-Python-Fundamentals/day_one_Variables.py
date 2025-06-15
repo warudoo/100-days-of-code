6f5d99f019b0e72aad2a7ebfbea577cd478b8913
warud <muhammadsalwarud@gmail.com>
2025-06-15 16:18:56 +0700

    feat: study about how to declare varibles on python

--- 01-Python-Fundamentals/day_one_Variables.py
+++ 01-Python-Fundamentals/day_one_Variables.py
@@ -10,3 +10,33 @@ z = float(3)  # z will be 3.0
 print(type(x))
 print(type(y))
 print(type(z))
+
+#legal varibles on python
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(my_var)
+
+#illegal variable on python is when you using keyword python as a varible name
+
+#assign values to multiple varibles
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+# One Value to Multiple Variables
+x = y = z = "Orangeasasa"
+print(x)
+print(y)
+print(z)
+
+# Unpack a Collection
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
