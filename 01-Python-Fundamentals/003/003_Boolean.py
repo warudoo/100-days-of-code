a6e2cbcc6fe2c24ea91648de167f94d53edaa942
warud <muhammadsalwarud@gmail.com>
2025-06-17 23:19:52 +0700

    feat: Learn Boolean with F string

--- 01-Python-Fundamentals/003/003_Boolean.py
+++ 01-Python-Fundamentals/003/003_Boolean.py
@@ -1,7 +1,19 @@
-a = int(input("Enter a number for a: "))
-b = int(input("Enter a number for b: "))
+a = 30
+b = 20
 
 if a > b:
     print("a is greater than b")
 elif a < b:
     print("a is less than b")
+    
+# Example of using f-string with boolean values
+is_greater = a > b
+print(f"Is a greater than b? {is_greater}")
+# Example of using f-string with boolean expressions 
+is_equal = a == b
+print(f"Is a equal to b? {is_equal}")
+# Example of using boolean in a conditional statement
+if is_greater:
+    print("Yes, a is greater than b")
+else:
+    print("No, a is not greater than b")
